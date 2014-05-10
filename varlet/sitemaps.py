0e8778be5feaa58093ae1d0097722125fb5c77ba
Keryn Knight <keryn@kerynknight.com>
2014-05-10 14:35:33 +0100

    quick and dirty site map.

---
+++ varlet/sitemaps.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+import logging
+from django.contrib.sitemaps import Sitemap
+from .models import Page
+
+
+logger = logging.getLogger(__name__)
+
+
+class MinimalPageSitemap(Sitemap):
+
+    @property
+    def model(self):
+        raise NotImplementedError("Subclasses should assign a concrete model")
+
+    def items(self):
+        raise NotImplementedError("Subclasses should implement this")
+
+    def lastmod(self, obj):
+        return obj.modified
+
+    def changefreq(self, obj):
+        datediff = datetime.today() - obj.modified
+        if datediff.days < 3:
+            return 'daily'
+        if datediff.days <= 7:
+            return 'weekly'
+        return 'monthly'
+
+    def priority(self, obj):
+        """
+        The farther down the rabbit hole we go, the lest important the pages,
+        right?
+        """
+        final_priority = 1.0
+        freq = self.changefreq(obj=obj)
+        if freq == 'weekly':
+            final_priority = 0.8
+        elif freq == 'monthly':
+            final_priority = 0.6
+        return final_priority
+
+
+class PageSitemap(MinimalPageSitemap):
+    model = Page
+
+    def items(self):
+        return self.model.objects.all()
