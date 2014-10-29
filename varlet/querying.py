2a93a6c09cdfc472e35141bc151825a846b0ad48
Keryn Knight <keryn@kerynknight.com>
2014-10-29 17:56:41 +0000

    Add in a manager/queryset method covering the minimum values needed to create a concrete page

--- varlet/querying.py
+++ varlet/querying.py
@@ -1,8 +1,9 @@
+from django.core.exceptions import ValidationError
 from django.db.models import Manager
 from django.db.models.query import QuerySet
 
 
-class PageQuerySet(QuerySet):
+class MinimalPageQuerySet(QuerySet):
     def get_homepage(self):
         return self.get(is_homepage=True)
 
@@ -10,17 +11,37 @@ class PageQuerySet(QuerySet):
         return self.filter(is_homepage=False)
 
 
-class PageManager(Manager):
+class MinimalPageManager(Manager):
     use_for_related_fields = True
 
     def get_query_set(self):
-        return PageQuerySet(self.model, using=self._db)
+        return MinimalPageQuerySet(self.model, using=self._db)
 
     def get_queryset(self):
-        return PageQuerySet(self.model, using=self._db)
+        return MinimalPageQuerySet(self.model, using=self._db)
 
     def get_homepage(self):
         return self.get_queryset().get_homepage()
 
     def not_homepage(self):
         return self.get_queryset().not_homepage()
+
+
+class PageQuerySet(MinimalPageQuerySet):
+    def create_page(self, title, template, slug):
+        possible_templates = tuple(self.model._get_template_choices())
+        if template not in possible_templates:
+            raise ValidationError("Invalid template")
+        return self.create(title=title, template=template, slug=slug)
+
+
+class PageManager(MinimalPageManager):
+    def get_query_set(self):
+        return PageQuerySet(self.model, using=self._db)
+
+    def get_queryset(self):
+        return PageQuerySet(self.model, using=self._db)
+
+    def create_page(self, title, template, slug):
+        return self.get_queryset().create_page(title=title, template=template,
+                                               slug=slug)
