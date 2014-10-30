8d6996348dcbb7c6b6e16d4e16bd2488049f562a
Keryn Knight <keryn@kerynknight.com>
2014-10-30 14:34:27 +0000

    Fix templatetag introduced in f1769d2cfb2eaaf32470ee0ff1f8a3ff9944cfb2

--- varlet/templatetags/get_page.py
+++ varlet/templatetags/get_page.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
-from classytags.arguments import Argument
+import logging
+from django.conf import settings
+from classytags.arguments import Argument, StringArgument
 from classytags.core import Options
 from classytags.helpers import AsTag
 from django import template
 from varlet.models import Page
 
+
 register = template.Library()
+logger = logging.getLogger(__name__)
 
 
 class GetPage(AsTag):
@@ -18,7 +22,7 @@ class GetPage(AsTag):
     """
     name = 'get_page'
     options = Options(
-        Argument('slug', resolve=True, required=True),
+        StringArgument('slug', resolve=True, required=True),
         'as',
         Argument('varname', resolve=False, required=True),
     )
@@ -26,6 +30,11 @@ class GetPage(AsTag):
     def get_value(self, context, slug):
         try:
             return Page.objects.get(slug=slug)
-        except Page.DoesNotExist, Page.MultipleObjectsReturned:
+        except (Page.DoesNotExist, Page.MultipleObjectsReturned) as e:
+            debug_enabled = settings.DEBUG and settings.TEMPLATE_DEBUG
+            if debug_enabled and settings.DEBUG_PROPAGATE_EXCEPTIONS:
+                raise
+            logger.error("Page for slug: {slug!s} isn't in the "
+                         "database".format(slug=slug), exc_info=1)
             return None
 register.tag(GetPage)
