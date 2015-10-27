7c8910e91e9b1d7162ff7f213d4300cc7d17d4ba
Keryn Knight <keryn@kerynknight.com>
2015-10-27 20:59:31 +0000

    Allow pulling out individual (correct) url() definitions.

--- varlet/urls.py
+++ varlet/urls.py
@@ -2,12 +2,13 @@
 from django.conf.urls import url
 from .views import PageDetail, Homepage
 
-urlpatterns = (
-    url(regex=r'^(?P<slug>[-\w]+)/$',
-        view=PageDetail.as_view(),
-        name='view'),
 
-    url(regex=r'^$',
-        view=Homepage.as_view(),
-        name='index'),
-)
+page_view = url(regex=r'^(?P<slug>[-\w]+)/$',
+                view=PageDetail.as_view(),
+                name='view')
+
+page_index = url(regex=r'^$',
+                 view=Homepage.as_view(),
+                 name='index')
+
+urlpatterns = (page_view, page_index)
