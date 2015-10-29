9f66e7ff3f4d2663352e3736b7c58c2c4a2f45b4
Keryn Knight <keryn@kerynknight.com>
2015-10-29 12:34:01 +0000

    add a 302 to whatever the correct URL will be for the demo version.

--- test_urls.py
+++ test_urls.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 from django.contrib import admin
 from varlet import named_urls
 from varlet import get_version
+from django.views.generic import RedirectView
 
 
-urlpatterns = patterns('',
-    url(r'^admin_mountpoint/', include(admin.site.urls)),
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^page/{version}/'.format(version=get_version()),
         include(named_urls)),
-)
+    url(r'^$', RedirectView.as_view(permanent=False,
+                                    pattern_name='pages:index')),
+]
