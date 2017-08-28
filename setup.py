a7bd8a0239646f63d9556f002a7acd1242e593c6
Keryn Knight <keryn@kerynknight.com>
2017-08-28 11:55:16 +0100

    Drop non-LTS checks, because I don't care.

--- setup.py
+++ setup.py
@@ -91,6 +91,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Framework :: Django",
+        "Framework :: Django :: 1.11",
         "Framework :: Django :: 1.10",
         "Framework :: Django :: 1.8",
         "Framework :: Django :: 1.9",
