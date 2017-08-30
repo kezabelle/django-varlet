3e29e9eeae6d1a6e9f075902dc8cd5e37afde7fa
Keryn Knight <keryn@kerynknight.com>
2017-08-30 11:14:28 +0100

    Make sure the sitemap priority/frequency work

--- varlet/tests/test_sitemaps.py
+++ varlet/tests/test_sitemaps.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+
+from datetime import timedelta
 import pytest
 import swapper
 from django.contrib.sitemaps.views import sitemap
+from django.utils import timezone
 from django.utils.encoding import force_text
 
 from varlet.sitemaps import PageSitemap
@@ -18,3 +21,15 @@ def test_rendering_sitemaps(rf):
     assert '<loc>http://example.com/3/</loc>' in force_text(response.content)
     assert '<loc>http://example.com/2/</loc>' in force_text(response.content)
     assert '<loc>http://example.com/1/</loc>' in force_text(response.content)
+
+
+@pytest.mark.parametrize("recently,priority", (
+    (timezone.now() - timedelta(minutes=60), 1.0),
+    (timezone.now() - timedelta(days=10), 0.8),
+    (timezone.now() - timedelta(days=20), 0.6),
+))
+def test_rendering_sitemaps_highest_priorty(recently, priority):
+    Page = swapper.load_model('varlet', 'Page')
+    page1 = Page(url='test', template="admin/filter.html", modified=recently)
+    sm = PageSitemap()
+    assert sm.priority(obj=page1) == priority
